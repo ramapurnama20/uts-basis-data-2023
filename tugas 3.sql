50a8b9bbfe266301f5e586e52d8ff4093627aa3c
ramapurnama20 <129568907+ramapurnama20@users.noreply.github.com>
2023-11-09 01:28:17 +0700

    Add files via upload

---
+++ tugas 3.sql
@@ -0,0 +1,9 @@
+INSERT INTO `tugas 3` (`id`, `nama`) VALUES (2022, 'jefry');
+INSERT INTO `tugas 3` (`id`, `nama`) VALUES (2023, 'lusy');
+INSERT INTO `tugas 3` (`id`, `nama`) VALUES (2024, 'luxy');
+INSERT INTO `tugas 3` (`id`, `nama`, `jurusan`, `kampus`) VALUES (123, 'rama', 'informatika', 'tangerang');
+INSERT INTO `tugas 3` (`id`, `nama`, `jurusan`, `kampus`) VALUES (124, 'teo', 'sosiologi', 'jakarta');
+INSERT INTO `tugas 3` (`id`, `nama`, `jurusan`, `kampus`) VALUES (125, 'ratna', 'hukum', 'tangerang');
+INSERT INTO `tugas 3` (`code`, `matkul`) VALUES (543, 'informatika');
+INSERT INTO `tugas 3` (`code`, `matkul`) VALUES (542, 'sosiologi');
+INSERT INTO `tugas 3` (`code`, `matkul`) VALUES (541, 'hukum');
