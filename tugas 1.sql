856e001441956bca52cc2b5bde52dee40bf531b3
ramapurnama20 <129568907+ramapurnama20@users.noreply.github.com>
2023-11-09 01:26:11 +0700

    Add files via upload

---
+++ tugas 1.sql
@@ -0,0 +1,16 @@
+CREATE TABLE mahasiswa (
+id int,
+nama VARCHAR(255),
+jurusan VARCHAR(255),
+kampus VARCHAR(225)
+);
+
+CREATE TABLE dosen (
+id int,
+nama VARCHAR(255)
+);
+
+CREATE TABLE matakuliah (
+code int,
+matkul VARCHAR(255)
+);
